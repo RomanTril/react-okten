716f05d2ee38fa087da99c821d6a8152aa113cd5
RomanTril <roman.tril1993@gmail.com>
2022-09-25 20:36:27 +0300

    lessons5ad

--- lesson5/src/components/posts/Posts.js
+++ lesson5/src/components/posts/Posts.js
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react';
 import {useParams} from "react-router-dom";
 
 import {postsService} from "../../services";
-import {Post} from "../../post/Post";
+import {Post} from "../post/Post";
 
 
 const Posts = () => {
